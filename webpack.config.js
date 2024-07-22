d35dfb3f805f3750af68e453879d46b87ab4aff9
Nirajan thapa <nnirajanthapaa@gmail.com>
2024-07-22 10:10:15 +0545

    ts-loader use

--- webpack.config.js
+++ webpack.config.js
@@ -13,17 +13,10 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(ts|tsx|js)$/,
+        test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
-          options: {
-            presets: [
-              "@babel/preset-env",
-              "@babel/preset-react",
-              "@babel/preset-typescript",
-            ],
-          },
+          loader: "ts-loader",
         },
       },
       {
