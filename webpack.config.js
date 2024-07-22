8716cc26299c42d4c6059d4a406095bb0f928c2e
Nirajan thapa <nnirajanthapaa@gmail.com>
2024-07-22 09:54:00 +0545

    name changed

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "index.js",
     libraryTarget: "umd",
-    library: "webpack-sdk",
+    library: "webpacklearning-sdk",
     umdNamedDefine: true,
   },
   module: {
